8f9647a642ac13cf4dfbe7e8462e4b18a91f293b
Matheus Bastos <matheus.mab432@gmail.com>
2026-08-16 15:43:08 -0300

    chore: add basic arrow key input handling loops

--- crates/snake-game/src/main.rs
+++ crates/snake-game/src/main.rs
@@ -1,33 +1,76 @@
-use std::io::{self, Write};
+use std::{
+    io::{self, Write},
+    thread,
+};
 
-pub const QUIT: &str = "q";
+use crossterm::{
+    event::{self, Event, KeyCode},
+    terminal::{disable_raw_mode, enable_raw_mode},
+};
+
+pub const QUIT: char = 'q';
 
 fn main() -> io::Result<()> {
     // TODO: impl the newtypes and instantiate the snek
+    // TODO: use Arc<Mutex<T>> ?
     // let mut snake = Snake {hp:};
     // let mut snake_hp = 5_u32;
-    loop {
-        // TODO: add crossterm to handle input events
-        print!("enter move ['{QUIT}' to quit]: ");
-        io::stdout().flush()?;
-        let move_to = {
-            let mut buffer = String::new();
-            io::stdin().read_line(&mut buffer)?;
-            buffer.trim_end().to_owned()
-        };
-        println!("move is: {}", move_to);
 
-        if move_to.as_str() == QUIT {
-            println!("bye :j");
-            break Ok(());
+    // TODO: use newtype for directional inputs
+    let (input_tx, input_rx) = std::sync::mpsc::channel();
+
+    enable_raw_mode()?;
+    println!("enter move ['{QUIT}' to quit]: ");
+    let input_handle = thread::spawn(move || {
+        loop {
+            // TODO: this is blocking. try to have a loop with tx/rx before using async EventStream
+            if let Ok(Event::Key(key)) = event::read().inspect_err(|err| {
+                eprintln!("error on event read: {}", err);
+            }) {
+                println!("sending...");
+                // best effort to send key
+                let _ = input_tx.send(key);
+            }
+        }
+    });
+    disable_raw_mode()?;
+
+    let game_loop_handle = thread::spawn(move || {
+        // TODO: without async i think 2 loops are necessary since input_rx can't listen on a closed channel, review for a simpler solution later
+        loop {
+            println!("receving...");
+            match input_rx.recv() {
+                Ok(key) => match key.code {
+                    arrow_key_code @ (KeyCode::Up
+                    | KeyCode::Down
+                    | KeyCode::Left
+                    | KeyCode::Right) => {
+                        println!("pressed direction: {}", arrow_key_code);
+                    }
+                    KeyCode::Char(QUIT) => break,
+                    _ => {}
+                },
+                Err(err) => {
+                    eprintln!("{}", err);
+                }
+            }
         }
         // snake_hp = snake_hp.saturating_sub(3);
         // println!("snake_hp: {}", snake_hp);
         // if snake_hp == 0 {
         //     break Ok(());
         // }
+    });
+    let handles = [input_handle, game_loop_handle];
+    for handle in handles {
+        handle.join().unwrap();
     }
+
+    Ok(())
 }
+
+// TODO: write and make it receive tx
+// fn handle_inputs(...)
 
 // TODO: move any 'mod's here to own files or snake-core once they get complex enough
 mod snake {
